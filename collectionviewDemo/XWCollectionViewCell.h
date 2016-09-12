11e2d994c7bf053bb9bd07036eb318f3f7bcf674
weixuewu <312206577@qq.com>
2016-09-12 10:05:15 +0800

    first commit

---
+++ collectionviewDemo/XWCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  XWCollectionViewCell.h
+//  collectionviewDemo
+//
+//  Created by weixuewu on 16/9/1.
+//  Copyright © 2016年 weixuewu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ collectionviewDemo/XWCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  XWCollectionViewCell.m
+//  collectionviewDemo
+//
+//  Created by weixuewu on 16/9/1.
+//  Copyright © 2016年 weixuewu. All rights reserved.
+//
+
+#import "XWCollectionViewCell.h"
+
+@implementation XWCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
